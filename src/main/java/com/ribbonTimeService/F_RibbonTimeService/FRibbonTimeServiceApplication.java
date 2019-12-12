6287b8b2e440ba3808a5a777586761c209d73888
Prabhat Kumar <prabhat.kumar05@nagarro.com>
2019-12-12 17:04:23 +0530

    first commit

---
+++ src/main/java/com/ribbonTimeService/F_RibbonTimeService/FRibbonTimeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ribbonTimeService.F_RibbonTimeService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FRibbonTimeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FRibbonTimeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ribbonTimeService/F_RibbonTimeService/FRibbonTimeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ribbonTimeService.F_RibbonTimeService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FRibbonTimeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
