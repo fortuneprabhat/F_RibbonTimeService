3a7f62bca7b76cbc8e73b91b3a05d50e792dddd3
Prabhat Kumar <prabhat.kumar05@nagarro.com>
2020-04-10 01:46:09 +0530

    property modified

--- src/main/java/com/ribbonTimeService/F_RibbonTimeService/FRibbonTimeServiceApplication.java
+++ src/main/java/com/ribbonTimeService/F_RibbonTimeService/FRibbonTimeServiceApplication.java
@@ -1,13 +1,28 @@
 package com.ribbonTimeService.F_RibbonTimeService;
 
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@RestController
 public class FRibbonTimeServiceApplication {
 
+	@Value("${server.port}")
+	private int port;
 	public static void main(String[] args) {
 		SpringApplication.run(FRibbonTimeServiceApplication.class, args);
+	}
+	
+	@GetMapping
+	public String getTime() {
+		return ("current time :"+new Date().toString()+ "/n Running on port:"+port);
 	}
 
 }
